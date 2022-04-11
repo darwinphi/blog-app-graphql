ebf50888b8642e4df912673c40352898d772b082
Darwin Manalo <darwinmanalophi@gmail.com>
2022-04-11 16:58:54 +0800

    Add auth logic to update and delete post, Add util

--- src/resolvers/Mutation/post.ts
+++ src/resolvers/Mutation/post.ts
@@ -1,5 +1,6 @@
 import { Post, Prisma } from "@prisma/client";
 import { Context } from "../../index";
+import { canUserMutatePost } from "../../utils/canUserMutatePost";
 
 interface PostArgs {
   post: {
@@ -57,8 +58,27 @@ export const postResolvers = {
   postUpdate: async (
     _: any,
     { postId, post }: { postId: string; post: PostArgs["post"] },
-    { prisma }: Context
+    { prisma, userInfo }: Context
   ): Promise<PostPayloadType> => {
+    if (!userInfo) {
+      return {
+        userErrors: [
+          {
+            message: "Forbidden access (Unauthenticated)",
+          },
+        ],
+        post: null,
+      };
+    }
+
+    const error = await canUserMutatePost({
+      userId: userInfo.userId,
+      postId: Number(postId),
+      prisma,
+    });
+
+    if (error) return error;
+
     const { title, content } = post;
 
     if (!title && !content) {
@@ -97,8 +117,27 @@ export const postResolvers = {
   postDelete: async (
     _: any,
     { postId }: { postId: string },
-    { prisma }: Context
+    { prisma, userInfo }: Context
   ): Promise<PostPayloadType> => {
+    if (!userInfo) {
+      return {
+        userErrors: [
+          {
+            message: "Forbidden access (Unauthenticated)",
+          },
+        ],
+        post: null,
+      };
+    }
+
+    const error = await canUserMutatePost({
+      userId: userInfo.userId,
+      postId: Number(postId),
+      prisma,
+    });
+
+    if (error) return error;
+
     const post = await prisma.post.findUnique({
       where: {
         id: Number(postId),
