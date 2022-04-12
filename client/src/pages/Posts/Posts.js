2919f3317b9fa930902cb1abc0ac30f18a5b8918
Darwin Manalo <darwinmanalophi@gmail.com>
2022-04-12 11:21:03 +0800

    Add GET_POSTS query

--- client/src/pages/Posts/Posts.js
+++ client/src/pages/Posts/Posts.js
@@ -1,6 +1,42 @@
+import { gql, useQuery } from "@apollo/client";
 import React from "react";
 import Post from "../../components/Post/Post";
 
+const GET_POSTS = gql`
+  query {
+    posts {
+      id
+      title
+      content
+      createdAt
+      user {
+        name
+      }
+    }
+  }
+`;
+
 export default function Posts() {
-  return <div></div>;
+  const { data, error, loading } = useQuery(GET_POSTS);
+  console.log(data, error, loading);
+
+  if (error) return <div>Error Page</div>;
+  if (loading) return <div>Loading...</div>;
+  const { posts } = data;
+
+  return (
+    <div>
+      {posts.map((post) => {
+        return (
+          <Post
+            title={post.title}
+            content={post.content}
+            date={post.createdAt}
+            id={post.id}
+            user={post.user.name}
+          />
+        );
+      })}
+    </div>
+  );
 }
